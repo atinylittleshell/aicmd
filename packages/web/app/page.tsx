9b3d529a4d9670b0b717ec85ddbcaf270bffcf99
Kun Chen <kunchenguid@gmail.com>
2023-04-02 12:56:46 -0700

    add more examples

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div className="flex-1 flex flex-col gap-8 items-center justify-center">
       <div className="prose">
-        <h1 className="text-center">Write shell commands using natural language.</h1>
+        <h1 className="text-center">Write shell commands using natural language</h1>
       </div>
       <div className="mockup-code w-full max-w-lg">
         <pre data-prefix=">">
@@ -10,6 +10,12 @@ export default function Page() {
         </pre>
         <pre data-prefix=">">
           <code>aicmd print hello world</code>
+        </pre>
+        <pre data-prefix=">">
+          <code>aicmd find all jpg files in the current directory</code>
+        </pre>
+        <pre data-prefix=">">
+          <code>aicmd revert last git commit</code>
         </pre>
       </div>
       <div className="prose">
