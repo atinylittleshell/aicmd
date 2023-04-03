658d589c341c5d26f32cd178a1d15311cab8592a
Kun Chen <kunchenguid@gmail.com>
2023-04-02 22:06:51 -0700

    add quick start page

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import { TbArrowRight } from 'react-icons/tb';
+
 export default function Page() {
   return (
     <div className="flex-1 flex flex-col gap-8 items-center justify-center">
@@ -5,9 +8,6 @@ export default function Page() {
         <h1 className="text-center">Write shell commands using natural language</h1>
       </div>
       <div className="mockup-code w-full max-w-lg">
-        <pre data-prefix=">">
-          <code>npm i -g aicmd</code>
-        </pre>
         <pre data-prefix=">">
           <code>aicmd undo the last two git commits</code>
         </pre>
@@ -18,14 +18,17 @@ export default function Page() {
           <code>Execute the command above? [y/N]</code>
         </pre>
       </div>
+      <Link href="/quick_start" className="btn btn-primary">
+        Quick Start <TbArrowRight />
+      </Link>
       <div className="prose">
         <ul>
           <li>
-            Runs on{' '}
+            Depends on{' '}
             <a href="https://nodejs.org/" target="_blank">
               Node.js
-            </a>
-            . Version 16+ required.
+            </a>{' '}
+            v16 or above.
           </li>
           <li>Always asks for confirmation before executing.</li>
           <li>Support all major OS and shells.</li>
