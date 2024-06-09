39f32ad78608fdc059027ed8aae477160e0fa57f
atinylittleshell <shell@atinylittleshell.me>
2024-06-08 21:30:04 -0700

    remove web

--- packages/web/components/ClientSideProviders/index.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { PropsWithChildren } from 'react';
-
-import { FirebaseContextProvider } from '../../hooks/FirebaseContext';
-
-export default function ClientSideProviders(props: PropsWithChildren) {
-  return <FirebaseContextProvider>{props.children}</FirebaseContextProvider>;
-}
